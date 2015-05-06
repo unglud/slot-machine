33a32b919a19a08a9e7231b1aebf11870f814158
unglued <alex@apus.ag>
2015-05-06 14:59:13 +0300

    add test dump

---
+++ tests/SlotMachine.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Created by Alexander 'unglued' Matrosov.
+ * Company: Apus Agency
+ * Site: http://www.apus.ag
+ * E-mail: alex@apus.ag
+ * Date: 03/05/15
+ * Copyright (c) 2006-2015 Apus Agency
+ */
+namespace Unglued\SlotMachine;
+
+
+class SlotMachineTest extends \PHPUnit_Framework_TestCase {
+
+
+}
